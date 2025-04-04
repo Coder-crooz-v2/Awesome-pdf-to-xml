c7aaaa6b9667c738f573d6a3865673bbba0da404
Coder-crooz-v2 <chaitalibhawal801@gmail.com>
2025-04-05 03:18:52 +0530

    fix: update cookie domain setting for production environment to Vercel URL

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -109,7 +109,7 @@ const loginUser = asyncHandler(async (req, res) =>{
         secure: true,
         sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
         maxAge: 24 * 60 * 60 * 1000,
-        domain: process.env.NODE_ENV === 'production' ? '.onrender.com' : 'localhost'
+        domain: process.env.NODE_ENV === 'production' ? 'https://awesome-pdf-to-xml.vercel.app' : 'localhost'
     }
 
     return res
