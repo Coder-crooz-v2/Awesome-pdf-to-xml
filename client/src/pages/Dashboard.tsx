e9636286f80fe83d9746dbb0aaa2d5e6a472316a
Coder-crooz-v2 <chaitalibhawal801@gmail.com>
2025-04-05 02:32:39 +0530

    bug fixes

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -50,7 +50,6 @@ import {
 } from "@/components/ui/alert-dialog";
 import axios from "axios";
 import FileViewer from "@/components/dashboard/FileViewer";
-import { useNavigate } from "react-router-dom";
 
 declare global {
   interface FileConstructor {
@@ -88,7 +87,6 @@ const Dashboard = () => {
   const [fileToDelete, setFileToDelete] = useState<Document | null>(null);
   
   const dispatch = useDispatch<AppDispatch>();
-  const navigate = useNavigate();
 
   const { documents, loading } = useSelector((state: RootState) => state.documents);
   const { history } = useSelector((state: RootState) => state.user);
