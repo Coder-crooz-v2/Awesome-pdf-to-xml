9d1f22db14e7c72e6b94e08939047cf75276b6ae
Coder-crooz-v2 <chaitalibhawal801@gmail.com>
2025-04-05 14:51:13 +0530

    fix: add secure configuration to Cloudinary setup

--- server/src/utils/cloudinary.js
+++ server/src/utils/cloudinary.js
@@ -5,7 +5,8 @@ import fs from "fs"
 cloudinary.config({ 
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
   api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: process.env.CLOUDINARY_API_SECRET 
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: process.env.NODE_ENV === "production" ? true : false
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
